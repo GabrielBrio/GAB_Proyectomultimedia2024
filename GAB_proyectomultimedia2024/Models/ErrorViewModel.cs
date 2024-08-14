b9a6eaff2c44d2cb6ce9dca8f90692119f9ab3d9
GabrielBrio <gabriel.chavez@alumnos.upla.cl>
2024-08-14 08:59:39 -0400

    Agregar archivos de proyecto.

---
+++ GAB_proyectomultimedia2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GAB_proyectomultimedia2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
